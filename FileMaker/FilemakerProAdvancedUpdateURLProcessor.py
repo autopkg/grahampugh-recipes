723caff1a10bd232f67fd17fead3fcae78114605
Elliot Jordan <elliot@elliotjordan.com>
2019-09-01 16:59:44 -0700

    Standardize indentation

--- FileMaker/FilemakerProAdvancedUpdateURLProcessor.py
+++ FileMaker/FilemakerProAdvancedUpdateURLProcessor.py
@@ -164,7 +164,7 @@ class FilemakerProAdvancedUpdateURLProcessor(Processor):
         return update
 
     def version_matcher(self, url):
-       	fname = os.path.basename(urlparse.urlsplit(url).path)
+        fname = os.path.basename(urlparse.urlsplit(url).path)
         version_match = re.search(r"([0-9]{2}.[0-9]{0,2}.[0-9]{0,2})", fname)
         if version_match == None:
             raise ProcessorError("Something went wrong matching FMP update to full version.")
