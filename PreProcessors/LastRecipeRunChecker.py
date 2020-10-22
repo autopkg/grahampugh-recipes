f5bdba44fcd2ccd6104f001617efa742cd991894
Graham R Pugh <graham.pugh@id.ethz.ch>
2020-10-22 22:13:22 +0200

    lastreciperunchecker processor

---
+++ PreProcessors/LastRecipeRunChecker.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+#
+# 2019 Graham R Pugh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""See docstring for LastRecipeRunChecker class"""
+
+import json
+import os
+import sys
+
+import os.path
+from autopkglib import Processor, ProcessorError  # pylint: disable=import-error
+
+
+__all__ = ["LastRecipeRunChecker"]
+
+
+class LastRecipeRunChecker(Processor):
+    """An AutoPkg pre-processor which reads the output from the LastRecipeRunResult processor from a different AutoPkg recipe, so that they can be used in the foillowing processes."""
+
+    input_variables = {
+        "recipeoverride_identifier": {
+            "description": "The identifier of the recipe from which the information is required.",
+            "required": True,
+        },
+        "cache_dir": {
+            "description": "Path to the cache dir.",
+            "required": False,
+            "default": "~/Library/AutoPkg/Cache",
+        },
+        "info_file": {
+            "description": ("Name of inmput file."),
+            "required": False,
+            "default": "latest_version.json",
+        },
+    }
+
+    output_variables = {
+        "version": {"description": ("The current package version."),},
+        "CATEGORY": {"description": ("The package category."),},
+        "SELF_SERVICE_DESCRIPTION": {"description": ("The self-service description."),},
+        "pkg_path": {"description": ("the package path."),},
+    }
+
+    description = __doc__
+
+    def get_latest_recipe_run_info(self, cache_dir, identifier, info_file):
+        """get information from the output files of a LastRecipeRunResult processor"""
+        try:
+            info_filepath = os.path.join(cache_dir, identifier, info_file)
+            with open(info_filepath, "r") as fp:
+                data = json.load(fp)
+        except (IOError, ValueError):
+            raise ProcessorError("No package or version information found")
+        else:
+            return data
+
+    def main(self):
+        identifier = self.env.get("recipeoverride_identifier")
+        cache_dir = os.path.expanduser(self.env.get("cache_dir"))
+        info_file = self.env.get("info_file")
+
+        version_found = False
+
+        # make sure all the values were obtained from the file
+        data = self.get_latest_recipe_run_info(cache_dir, identifier, info_file)
+        try:
+            self.env["version"] = data["version"]
+            self.env["pkg_path"] = data["pkg_path"]
+        except ValueError:
+            raise ProcessorError("No package or version information found")
+
+        # make sure the package actually exists
+        if not os.path.exists(self.env["pkg_path"]):
+            raise ProcessorError(
+                "Package does not exist: {}".format(self.env["pkg_path"])
+            )
+
+        self.output("Package: {}".format(self.env["pkg_path"]))
+        self.output("Version: {}".format(self.env["version"]))
+
+
+if __name__ == "__main__":
+    PROCESSOR = LastRecipeRunChecker()
+    PROCESSOR.execute_shell()
