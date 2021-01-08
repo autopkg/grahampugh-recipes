c5313eefdf00f7fe965d99a0adbf9c3a847570e3
Graham R Pugh <graham.pugh@id.ethz.ch>
2021-01-08 11:44:57 +0100

    writes whether a package was changed to the json

--- PreProcessors/LastRecipeRunChecker.py
+++ PreProcessors/LastRecipeRunChecker.py
@@ -51,6 +51,14 @@ class LastRecipeRunChecker(Processor):
         "license_key": {"description": ("The outputted value for license_key.")},
         "pkg_path": {"description": ("the package path.")},
         "pkg_name": {"description": ("the package name.")},
+        "pkg_uploaded": {
+            "description": ("whether a package was uploaded on the last run or not.")
+        },
+        "pkg_metadata_uploaded": {
+            "description": (
+                "whether package metadata was updated on the last run or not."
+            )
+        },
         "PKG_CATEGORY": {"description": ("The package category.")},
         "LAST_RUN_POLICY_NAME": {"description": ("The policy_name.")},
         "SELFSERVICE_DESCRIPTION": {"description": ("The self-service description.")},
@@ -79,6 +87,8 @@ class LastRecipeRunChecker(Processor):
         self.env["version"] = data["version"]
         self.env["license_key"] = data["license_key"]
         self.env["pkg_name"] = data["pkg_name"]
+        self.env["pkg_uploaded"] = data["pkg_uploaded"]
+        self.env["pkg_metadata_uploaded"] = data["pkg_metadata_uploaded"]
         if not self.env["version"] or not self.env["pkg_name"]:
             raise ProcessorError("No package or version information found")
         self.env["pkg_path"] = data["pkg_path"]
@@ -101,6 +111,8 @@ class LastRecipeRunChecker(Processor):
         self.output(f"Policy name: {data['policy_name']}")
         self.output(f"Self Service Description: {data['self_service_description']}")
         self.output(f"License Key: {data['license_key']}")
+        self.output(f"Package uploaded: {data['pkg_uploaded']}")
+        self.output(f"Package metadata updated: {data['pkg_metadata_uploaded']}")
 
 
 if __name__ == "__main__":
