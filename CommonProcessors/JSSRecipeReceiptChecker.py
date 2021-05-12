e566adee5000a770f275f6edb03c53ef0370b0c9
Zack T <MLBZ521@gmail.com>
2021-05-12 13:47:00 -0700

    Refactored + Getting Additional JSSImporter Details
    * Refactored code
    * Getting additional JSSImporter details from the receipt

--- CommonProcessors/JSSRecipeReceiptChecker.py
+++ CommonProcessors/JSSRecipeReceiptChecker.py
@@ -24,6 +24,7 @@ except:
 
 from glob import iglob
 from os.path import expanduser, getmtime, exists
+
 from autopkglib import Processor, ProcessorError  # pylint: disable=import-error
 
 
@@ -44,7 +45,7 @@ class JSSRecipeReceiptChecker(Processor):
                 "Assumes the Recipe ID is in the format of:  "
                 "   local.jss.<name>"
             ),
-            "required": False,
+            "required": False
         },
         "recipe_id": {
             "description": (
@@ -56,119 +57,211 @@ class JSSRecipeReceiptChecker(Processor):
         "cache_dir": {
             "description": "Path to the cache dir.",
             "required": False,
-            "default": "~/Library/AutoPkg/Cache",
-        },
+            "default": "~/Library/AutoPkg/Cache"
+        }
     }
 
     output_variables = {
-        "version": {"description": "The current package version."},
-        "version_regex": {"description": "The current package version regex."},
-        "CATEGORY": {"description": "The package category."},
-        "SELF_SERVICE_DESCRIPTION": {"description": "The self-service description."},
-        "pkg_path": {"description": "the package path."},
-        "license_key": {"description": "the license key."},
+        "version": {
+            "description": "The current package version."
+        },
+        "version_regex": {
+            "description": "The current package version regex."
+        },
+        "CATEGORY": {
+            "description": "The package category."
+        },
+        "SELF_SERVICE_DESCRIPTION": {
+            "description": "The self service description."
+        },
+        "pkg_path": {
+            "description": "The package path."
+        },
+        "license_key": {
+            "description": "The license key."
+        },
+        "SELF_SERVICE_ICON": {
+            "description": "The self service icon."
+        },
+        "package_notes": {
+            "description": "The package notes."
+        },
+        "prod_name": {
+            "description": "The prod name of the Policy."
+        },
+        "PARENT_RECIPES": {
+            "description": "The parent recipes, used to locate the self service icon."
+        }
     }
 
     description = __doc__
 
-    def get_latest_receipt(self, cache_dir, name, receipt_number):
-        """name of receipt with the highest version number"""
+
+    def get_recipe_receipts(self, cache_dir, name):
+        """Get the receipts for the passed recipe name.
+
+        Args:
+            cache_dir (str): Path to the AutoPkg cache directory.
+            name (str): Recipe NAME or ID.
+
+        Raises:
+            ProcessorError: Unable to locate receipts for the provided recipe.
+
+        Returns:
+            list: A list of recipe receipts.
+        """
+
         self.output(
             "Checking for receipts in folder {}/{}".format(cache_dir, name))
-        files = list(iglob("{}/*{}/receipts/*.plist".format(cache_dir, name)))
-        files.sort(key=lambda x: getmtime(x), reverse=True)
-        return files[receipt_number]
+
+        try:
+            files = list(iglob("{}/*{}/receipts/*.plist".format(
+                cache_dir, name)))
+            files.sort(key=lambda x: getmtime(x), reverse=True)
+            return files
+
+        except IOError:
+            raise ProcessorError("No receipts found!")
+
 
     def main(self):
-        """do the main thing"""
+        """Find the latest receipt that contains all 
+            the information we're looking for.
+
+        Raises:
+            ProcessorError: Proper input variables must be supplied.
+            ProcessorError: Package does not exist at the path found.
+            ProcessorError: Unable to locate a receipt 
+                            with the desired information.
+        """
+
         name = self.env.get("name")
         recipe_id = self.env.get("recipe_id")
         cache_dir = expanduser(self.env.get(
             "cache_dir", "~/Library/AutoPkg/Cache"))
         version_found = False
+        found_parent_recipes = False
 
         if not name:
             if recipe_id:
                 name = recipe_id
             else:
                 raise ProcessorError(
-                    "Either 'name' or 'recipe_name' must be provided.")
+                    "Either 'name' or 'recipe_id' must be provided.")
         else:
             name = "local.jss.{}".format(name)
 
-        receipt_number = 0
-        while not version_found:
+        receipts = self.get_recipe_receipts(cache_dir, name)
+
+        for receipt in receipts:
+
             try:
-                receipt = self.get_latest_receipt(cache_dir, name, receipt_number)
-            except IOError:
-                raise ProcessorError("No receipt found!")
 
-            self.output("Receipt: {}".format(receipt))
+                self.output("Scanning receipt:  {}".format(receipt))
 
-            with open(receipt, 'rb') as plist_receipt:
-                plist = plistReader(plist_receipt)
+                with open(receipt, 'rb') as plist_receipt:
+                    plist = plistReader(plist_receipt)
 
-            i = 0
-            while i < len(plist):
-                try:
-                    processor = plist[i]["Processor"]
-                    if processor == "ch.ethz.id.check.itshop/ITShopUpdateChecker":
-                        license_key = plist[i]["Output"]["license_key"]
-                    if (
-                        processor
-                        == "com.github.grahampugh.recipes.commonprocessors/VersionRegexGenerator"
-                    ):
-                        version_regex = plist[i]["Output"]["version_regex"]
-                    if processor == "JSSImporter":
-                        version = plist[i]["Input"]["version"]
-                        if plist[i]["Input"]["pkg_path"] != "":
-                            pkg_path = plist[i]["Input"]["pkg_path"]
-                        category = plist[i]["Input"]["category"]
-                        self_service_description = plist[i]["Input"][
-                            "self_service_description"
-                        ]
-                        version_found = True
-                        break
-                except KeyError:
-                    self.output("No JSSImporter process in: {}".format(receipt))
-                i = i + 1
+                for step in plist:
 
-            # make sure all the values were obtained from the receipt
-            try:
-                self.env["version"] = version
-                self.output("Version: {}".format(version))
-                if pkg_path:
-                    self.env["pkg_path"] = pkg_path
+                    for k, v in step.items():
+
+                        if k == "Recipe input":
+                            parent_recipes = v.get("PARENT_RECIPES")
+                            found_parent_recipes = True
+
+                        if ( step.get(k) == 
+                            "ch.eth.id.check.itshop/ITShopUpdateChecker"):
+                            license_key = step.get("Output").get("license_key")
+
+                        if ( step.get(k) == 
+                            "com.github.grahampugh.recipes.commonprocessors/VersionRegexGenerator"):
+                            version_regex = step.get("Output").get("version_regex")
+
+                        if step.get(k) == "JSSImporter":
+
+                            jssimporter_input = step.get("Input")
+                            version = jssimporter_input.get("version")
+                            pkg_path = jssimporter_input.get("pkg_path")
+                            category = jssimporter_input.get("category")
+                            self_service_description = jssimporter_input.get(
+                                "self_service_description")
+                            self_service_icon = jssimporter_input.get(
+                                "self_service_icon")
+                            package_notes = jssimporter_input.get(
+                                "package_notes", "")
+                            prod_name = jssimporter_input.get("prod_name")
+                            version_found = True
+                    
+                        if found_parent_recipes and version_found:
+                            break
+
+                    else:
+                        continue
+
+                    break
+
                 else:
-                    self.env["pkg_path"] = ""
-                self.output("Package: {}".format(pkg_path))
-                self.env["CATEGORY"] = category
-                self.output("Category: {}".format(category))
-                self.env["SELF_SERVICE_DESCRIPTION"] = self_service_description
-                self.output(
-                    "Self Service Description: {}".format(self_service_description)
-                )
-                try:
-                    self.env["license_key"] = license_key
-                    self.output("License Key: {}".format(license_key))
-                except UnboundLocalError:
-                    self.env["license_key"] = ""
-                try:
-                    self.env["version_regex"] = version_regex
-                    self.output("Version Regex: {}".format(version_regex))
-                except UnboundLocalError:
-                    self.output(
-                        "No version_regex found in receipt - reverting to version"
-                    )
-                    self.env["version_regex"] = "^{}$".format(version)
-            except UnboundLocalError:
-                self.output("No version found in receipt")
-                receipt_number = receipt_number + 1
+                    continue
 
-        # make sure the package actually exists
-        if not exists(pkg_path):
-            raise ProcessorError("Package does not exist: {}".format(pkg_path))
-        # end
+                break
+                
+            except:
+                self.output("Missing required information...")
+                continue
+
+
+        self.env["version"] = version
+        self.output("Version:  {}".format(version), verbose_level=2)
+
+        self.env["pkg_path"] = pkg_path
+        self.output("Package:  {}".format(pkg_path), verbose_level=2)
+
+        self.env["CATEGORY"] = category
+        self.output("Category:  {}".format(category), verbose_level=2)
+
+        self.env["SELF_SERVICE_DESCRIPTION"] = self_service_description
+        self.output("Self Service Description:  {}".format(
+            self_service_description), verbose_level=2)
+
+        self.env["SELF_SERVICE_ICON"] = self_service_icon
+        self.output("Self Service Icon:  {}".format(
+            self_service_icon), verbose_level=2)
+
+        self.env["package_notes"] = package_notes
+        self.output("Package notes:  {}".format(
+            package_notes), verbose_level=2)
+
+        self.env["prod_name"] = prod_name
+        self.output("prod_name:  {}".format(prod_name), verbose_level=2)
+
+        self.env["PARENT_RECIPES"].extend(parent_recipes)
+        self.output("Parent Recipes:  {}".format(
+            parent_recipes), verbose_level=2)
+
+        try:
+            self.env["license_key"] = license_key
+            self.output("License Key:  {}".format(
+                license_key), verbose_level=2)
+        except UnboundLocalError:
+            self.env["license_key"] = ""
+
+        try:
+            self.env["version_regex"] = version_regex
+            self.output("Version Regex:  {}".format(
+                version_regex), verbose_level=2)
+        except UnboundLocalError:
+            self.output(
+                "No version_regex found in receipt - reverting to version", 
+                verbose_level=2)
+            self.env["version_regex"] = "^{}$".format(version)
+
+        if version_found:
+            # Make sure the package actually exists
+            if not exists(pkg_path):
+                raise ProcessorError("Package does not exist:  {}".format(pkg_path))
+        else:
+            raise ProcessorError("Unable to locate a receipt with version!")
 
 
 if __name__ == "__main__":
