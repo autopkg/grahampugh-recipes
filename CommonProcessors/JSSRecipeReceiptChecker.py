20734f7a75f71782149708f21336ed9bdc4acd6e
Zack T <MLBZ521@gmail.com>
2021-09-02 11:21:18 -0700

    Refactored code (again), plus additional variable support
    + Refactored code (again)
    + Added option to pull custom input variables that are unique to the environment
    + Added logic to check If receipt had an error, then skip that receipt
    + Made it even more obvious what steps are being parsed in the receipt
    + Improved the parse time for each step, if it matches a step that is being checked for, after parsing is complete, continue to the next step instead of checking additional if statements
    + Getting additional Input variables and Parent Recipe paths
    + Grabbing all JSSImporter variables (except for excluded ones) and setting the proper case
    + Added support for a custom processor (that I use) `InputVariableTextSubstituter` to ensure these variables have the proper values set

--- CommonProcessors/JSSRecipeReceiptChecker.py
+++ CommonProcessors/JSSRecipeReceiptChecker.py
@@ -22,6 +22,8 @@ try:
 except:
     from plistlib import readPlist as plistReader # Python 2
 
+import re
+
 from glob import iglob
 from os.path import expanduser, getmtime, exists
 
@@ -58,6 +60,13 @@ class JSSRecipeReceiptChecker(Processor):
             "description": "Path to the cache dir.",
             "required": False,
             "default": "~/Library/AutoPkg/Cache"
+        },
+        "custom_variables": {
+            "description": ( "An array of custom input variables that should be "
+                "pulled from the latest receipt.  These would generally be unique "
+                "to your environment."
+            ),
+            "required": False,
         }
     }
 
@@ -137,131 +146,127 @@ class JSSRecipeReceiptChecker(Processor):
 
         name = self.env.get("name")
         recipe_id = self.env.get("recipe_id")
-        cache_dir = expanduser(self.env.get(
-            "cache_dir", "~/Library/AutoPkg/Cache"))
+        custom_variables = self.env.get("custom_variables", [])
+        cache_dir = expanduser(self.env.get("cache_dir", "~/Library/AutoPkg/Cache"))
         version_found = False
         found_parent_recipes = False
 
-        if not name:
-            if recipe_id:
-                name = recipe_id
-            else:
-                raise ProcessorError(
-                    "Either 'name' or 'recipe_id' must be provided.")
-        else:
+        if name:
             name = "local.jss.{}".format(name)
+
+        elif recipe_id:
+            name = recipe_id
+
+        else:
+            raise ProcessorError("Either 'name' or 'recipe_id' must be provided.")
 
         receipts = self.get_recipe_receipts(cache_dir, name)
 
         for receipt in receipts:
 
             try:
-
                 self.output("Scanning receipt:  {}".format(receipt))
 
                 with open(receipt, 'rb') as plist_receipt:
                     plist = plistReader(plist_receipt)
 
+                if str(plist[-1].keys()) == "dict_keys(['RecipeError'])":
+                    self.output("  -> Skipping as this receipt had an error...")
+                    continue
+
                 for step in plist:
-
-                    for k, v in step.items():
-
-                        if k == "Recipe input":
-                            parent_recipes = v.get("PARENT_RECIPES")
-                            found_parent_recipes = True
-
-                        if ( step.get(k) == 
-                            "ch.eth.id.check.itshop/ITShopUpdateChecker"):
-                            license_key = step.get("Output").get("license_key")
-
-                        if ( step.get(k) == 
-                            "com.github.grahampugh.recipes.commonprocessors/VersionRegexGenerator"):
-                            version_regex = step.get("Output").get("version_regex")
-
-                        if step.get(k) == "JSSImporter":
-
-                            jssimporter_input = step.get("Input")
-                            version = jssimporter_input.get("version")
-                            pkg_path = jssimporter_input.get("pkg_path")
-                            category = jssimporter_input.get("category")
-                            self_service_description = jssimporter_input.get(
-                                "self_service_description")
-                            self_service_icon = jssimporter_input.get(
-                                "self_service_icon")
-                            package_notes = jssimporter_input.get(
-                                "package_notes", "")
-                            prod_name = jssimporter_input.get("prod_name")
-                            version_found = True
                     
-                        if found_parent_recipes and version_found:
-                            break
+                    if step.get("Recipe input"):
 
-                    else:
+                        recipe_input = step.get("Recipe input")
+                        parent_recipes = recipe_input.get("PARENT_RECIPES")
+
+                        if parent_recipes:
+                            found_parent_recipes = True
+                            self.env["PARENT_RECIPES"].extend(parent_recipes)
+                            self.env["PARENT_RECIPES"].append(recipe_input.get("RECIPE_DIR"))
+                            self.env["PARENT_RECIPES"].append(recipe_input.get("RECIPE_PATH"))
+                            self.output("Parent Recipes:  {}".format(
+                                self.env["PARENT_RECIPES"]), verbose_level=2)
+
+                        for key in custom_variables:
+                            self.env[key] = recipe_input.get(key)
+                            self.output("{}:  {}".format(key, self.env[key]), verbose_level=2)
+
+                        self.env["NAME"] = recipe_input.get("NAME")
+                        self.output("NAME:  {}".format(self.env["NAME"]), verbose_level=2)
+
                         continue
 
+                    if step.get("Processor"):
+
+                        if step.get("Processor") == "ch.eth.id.check.itshop/ITShopUpdateChecker":
+                            license_key = step.get("Output").get("license_key")
+                            continue
+
+                        if step.get("Processor") == \
+                            "com.github.grahampugh.recipes.commonprocessors/VersionRegexGenerator":
+                            version_regex = step.get("Output").get("version_regex")
+                            continue
+
+                        if re.search("InputVariableTextSubstituter", step.get("Processor"), re.IGNORECASE):
+                            processor_output = step.get("Output")
+                            key = processor_output.get("return_variable")
+                            value = processor_output.get("return_variable_value")
+                            self.output("{}:  {}".format(key, value), verbose_level=2)
+                            self.env[key] = value
+                            continue
+
+                        if re.search("JSSImporter", step.get("Processor"), re.IGNORECASE):
+                            jssimporter_input = step.get("Input")
+
+                            for key, value in jssimporter_input.items():
+
+                                # We don't want to pull these values, incase they're different.
+                                if key not in { 
+                                    "API_PASSWORD", "API_USERNAME", "JSS_REPOS", "JSS_URL", "JSS_VERIFY_SSL" }:
+
+                                    # Set the proper CASE for these variables
+                                    var_name = ( key if key in { "pkg_path", "prod_name", "version" } 
+                                        else key.upper() )
+
+                                    self.env[var_name] = value
+                                    self.output("{}:  {}".format(var_name, self.env[var_name]), verbose_level=2)
+
+                            if self.env["version"]:
+                                version_found = True
+
+                            continue
+
+                if found_parent_recipes and version_found:
                     break
 
                 else:
                     continue
 
-                break
-                
             except:
                 self.output("Missing required information...")
                 continue
 
-
-        self.env["version"] = version
-        self.output("Version:  {}".format(version), verbose_level=2)
-
-        self.env["pkg_path"] = pkg_path
-        self.output("Package:  {}".format(pkg_path), verbose_level=2)
-
-        self.env["CATEGORY"] = category
-        self.output("Category:  {}".format(category), verbose_level=2)
-
-        self.env["SELF_SERVICE_DESCRIPTION"] = self_service_description
-        self.output("Self Service Description:  {}".format(
-            self_service_description), verbose_level=2)
-
-        self.env["SELF_SERVICE_ICON"] = self_service_icon
-        self.output("Self Service Icon:  {}".format(
-            self_service_icon), verbose_level=2)
-
-        self.env["package_notes"] = package_notes
-        self.output("Package notes:  {}".format(
-            package_notes), verbose_level=2)
-
-        self.env["prod_name"] = prod_name
-        self.output("prod_name:  {}".format(prod_name), verbose_level=2)
-
-        self.env["PARENT_RECIPES"].extend(parent_recipes)
-        self.output("Parent Recipes:  {}".format(
-            parent_recipes), verbose_level=2)
-
         try:
             self.env["license_key"] = license_key
-            self.output("License Key:  {}".format(
-                license_key), verbose_level=2)
+            self.output("License Key:  {}".format(license_key), verbose_level=2)
         except UnboundLocalError:
             self.env["license_key"] = ""
 
         try:
             self.env["version_regex"] = version_regex
-            self.output("Version Regex:  {}".format(
-                version_regex), verbose_level=2)
+            self.output("Version Regex:  {}".format(version_regex), verbose_level=2)
         except UnboundLocalError:
-            self.output(
-                "No version_regex found in receipt - reverting to version", 
-                verbose_level=2)
-            self.env["version_regex"] = "^{}$".format(version)
+            self.output("No version_regex found in receipt - reverting to version", verbose_level=2)
+            self.env["version_regex"] = "^{}$".format(self.env["version"])
 
-        if version_found:
-            # Make sure the package actually exists
-            if not exists(pkg_path):
-                raise ProcessorError("Package does not exist:  {}".format(pkg_path))
-        else:
+        if not version_found:
             raise ProcessorError("Unable to locate a receipt with version!")
+
+        # Make sure the package actually exists
+        if not exists(self.env["pkg_path"]):
+            raise ProcessorError("Package does not exist:  {}".format(self.env["pkg_path"]))
 
 
 if __name__ == "__main__":
