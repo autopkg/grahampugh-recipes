354c37591bf48e7e6d47f47aed6cdd738febb9d8
Graham Pugh <graham.pugh@jamf.com>
2024-10-31 12:16:43 +0100

    TypeError

--- CommonProcessors/IconGenerator.py
+++ CommonProcessors/IconGenerator.py
@@ -301,10 +301,7 @@ class IconGenerator(DmgMounter):
                 else:
                     self.output("Resizing icon to 256px.")
                     icon = icon.resize((256, 256))
-            except Exception as ex:
-                template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-                message = template.format(type(ex).__name__, ex.args)
-                print(message)
+            except TypeError:
                 self.output("Resizing icon to 256px.")
                 icon = icon.resize((256, 256))
             icon.convert("RGBA")
